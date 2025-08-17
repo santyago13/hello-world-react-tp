8e57de0d745a4619f3cd4488b9f47bdf8850bea8
Santiago Robledo <santiddf9@gmail.com>
2025-08-17 17:43:31 -0300

    desisntale tailwinds e instale bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import Mensaje from "./components/Mensaje"
 
 function App() {
   return (
-    <main className="text-red-500">
+    <main className="m-4 text-center">
       <Mensaje></Mensaje>
     </main>
-  )
+  );
 }
 
 export default App
