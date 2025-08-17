9d55d8c504965175435ff6ffa397eb48554cc47a
Santiago Robledo <santiddf9@gmail.com>
2025-08-17 17:15:52 -0300

    agregue la estructura principal del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Mensaje from "./components/Mensaje"
+
+function App() {
+  return (
+    <main className="text-red-500">
+      <Mensaje></Mensaje>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Mensaje.jsx
@@ -0,0 +1,9 @@
+const Mensaje = () => {
+    return (
+        <div>
+            Hello World
+        </div>
+    );
+};
+
+export default Mensaje;
